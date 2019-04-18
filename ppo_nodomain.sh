88b8703d32aad40a3104e8ad2dc53cd493557838
Reinsborg <jreinsborg@gmail.com>
2019-04-18 09:19:42 +0200

    added ppo script for no domain version

---
+++ ppo_nodomain.sh
@@ -0,0 +1,9 @@
+./phase1.sh \
+    --state_is_pixels \
+    --explorer="ppo" \
+    --seen_weight=3.0 \
+    --chosen_weight=0.1 \
+    --high_score_weight=0.0 \
+    --max_hours=120 \
+    --max_compute_steps=300000000
+
